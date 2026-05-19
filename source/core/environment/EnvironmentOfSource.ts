2c62127075efa38033ad2123f14510e480a6428e
Rafał Majewski <goodheropl@gmail.com>
2026-05-19 22:44:46 +0000

    chore: Add an environment of source

---
+++ source/core/environment/EnvironmentOfSource.ts
@@ -0,0 +1,4 @@
+import type {ConfigurationOfEnvironmentOfSource} from "./fields/index.ts";
+export type EnvironmentOfSource = {
+	readonly configuration: ConfigurationOfEnvironmentOfSource;
+};
---
+++ source/core/environment/index.ts
@@ -0,0 +1,1 @@
+export type {EnvironmentOfSource} from "./EnvironmentOfSource.ts";
