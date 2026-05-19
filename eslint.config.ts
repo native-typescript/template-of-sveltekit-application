bf4e38ac5b2945c9eb51dae00816a43595d4a6bd
Rafał Majewski <goodheropl@gmail.com>
2026-05-19 22:45:26 +0000

    chore: Add Stylelint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1215,5 +1215,9 @@ export default [
 		files: [`./release-it.config.ts`],
 		rules: {"import-x/no-default-export": [`off`]},
 	},
+	{
+		files: [`./stylelint.config.ts`],
+		rules: {"import-x/no-default-export": [`off`]},
+	},
 	{ignores: [`./.git`, `./node_modules`]},
 ] as const satisfies readonly Linter.Config[];
