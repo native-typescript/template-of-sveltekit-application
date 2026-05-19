e01674a4093487cfe4ba90fb4eadbbf08a9f4727
Rafał Majewski <goodheropl@gmail.com>
2026-05-19 22:45:04 +0000

    chore: Add creating the environment of source from configuration

---
+++ source/core/environment/creating-from-configuration/create.ts
@@ -0,0 +1,8 @@
+import type {configuration_} from "../configuration/module.ts";
+import type {Environment} from "../Environment.ts";
+export function create(
+	configuration: configuration_.Configuration,
+): Environment {
+	const environment: Environment = {};
+	return environment;
+}
