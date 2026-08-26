05d7e10c580c41333e6b05b26759a6bbf04680c2
Rafał Majewski <goodheropl@gmail.com>
2026-08-26 08:05:09 +0000

    chore: Add a stub configuration of the environment of source

---
+++ source/core/environment/configuration/index.ts
@@ -0,0 +1,2 @@
+export type {Configuration} from "./Configuration.ts";
+export type * from "./fields/module.ts";
--- source/core/environment/index.ts
+++ source/core/environment/index.ts
@@ -1,2 +1,3 @@
+export type * from "./configuration/module.ts";
 export type {Environment} from "./Environment.ts";
 export type * from "./fields/module.ts";
